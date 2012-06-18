01d8a9fd4b65a2bc8f38de9b5807971d36d439ed
guitarguyger <ger_sharon@hotmail.com>
2012-06-19 00:19:47 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MusicShop::Application.config.secret_token = '156e8433fba7f3157d1f6f117e565016917c286fb3221f899dd0113ad5180485a405e841f753893cd9e4edb67243d2e712878be4a07e9f1748344d2c334007fd'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+MusicShop::Application.config.session_store :cookie_store, key: '_music_shop_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# MusicShop::Application.config.session_store :active_record_store
